c8a5400654ac3a4b8f50d7053a87d72327d0ddca
Carlos Morell <carlos.rolando.morell@gmail.com>
2022-08-07 19:54:22 -0400

    First Stable Version

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <app-registration></app-registration>
+    <app-registrations></app-registrations>
+  </div>
+</template>
+
+<script>
+import AppRegistration from "./components/AppRegistration.vue";
+import AppRegistrations from "./components/AppRegistrations.vue";
+
+export default {
+  data() {
+    return {
+      registrations: [],
+      users: [
+        { id: 1, name: "Max", registered: false },
+        { id: 2, name: "Anna", registered: false },
+        { id: 3, name: "Chris", registered: false },
+        { id: 4, name: "Sven", registered: false },
+      ],
+    };
+  },
+  computed: {
+    unregisteredUsers() {
+      return this.users.filter((user) => {
+        return !user.registered;
+      });
+    },
+  },
+  methods: {
+    userUnregistered(registration) {
+      const user = this.users.find((user) => {
+        return user.id == registration.userId;
+      });
+      user.registered = false;
+      this.registrations.splice(this.registrations.indexOf(registration), 1);
+    },
+  },
+  components: {
+    appRegistration: AppRegistration,
+    appRegistrations: AppRegistrations,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/AppRegistrations.vue
@@ -0,0 +1,76 @@
+<template>
+  <div id="registrations">
+    <div class="summary">
+      <h3>Registrations</h3>
+      <h5>Total: {{ total }}</h5>
+    </div>
+    <hr />
+    <div class="row" v-for="(registration, index) in registrations" :key="`registration-${index}`">
+      <h4>{{ registration.name }}</h4>
+      <span @click="unregister(registration)">(Unregister)</span>
+      <div class="date">{{ registration.date }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    unregister(registration) {
+      const user = this.$store.state.users.find((user) => {
+        return user.id == registration.userId;
+      });
+      user.registered = false;
+      this.$store.state.registrations.splice(this.$store.state.registrations.indexOf(registration), 1);
+    },
+  },
+  computed: {
+    registrations() {
+      return this.$store.state.registrations;
+    },
+    total() {
+      return this.$store.state.registrations.length;
+    },
+  },
+};
+</script>
+
+<style scoped>
+#registrations {
+  box-shadow: 1px 1px 2px 1px #ccc;
+  margin: 20px;
+  padding: 20px;
+  display: inline-block;
+  width: 300px;
+  vertical-align: top;
+  text-align: left;
+}
+
+.summary {
+  text-align: center;
+}
+
+.row h4 {
+  display: inline-block;
+  width: 30%;
+  margin: 0 0 10px 0;
+  box-sizing: border-box;
+}
+
+.row span {
+  width: 30%;
+  color: red;
+  cursor: pointer;
+}
+
+.row span:hover {
+  color: darkred;
+}
+
+.date {
+  display: inline-block;
+  width: 38%;
+  text-align: right;
+  box-sizing: border-box;
+}
+</style>
