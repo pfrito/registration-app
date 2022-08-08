ebeb832d7508a8191df5a917075cad54644c080f
Carlos Morell <carlos.rolando.morell@gmail.com>
2022-08-08 11:27:33 -0400

    I had two stores, fixed

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,13 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    registrations: [],
+    users: [
+      { id: 1, name: "Max", registered: false },
+      { id: 2, name: "Anna", registered: false },
+      { id: 3, name: "Chris", registered: false },
+      { id: 4, name: "Sven", registered: false },
+    ],
   },
   getters: {
   },
