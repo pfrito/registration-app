edf6e78cdd8b600309ec00f28da6da21c1030b48
Carlos Morell <carlos.rolando.morell@gmail.com>
2022-09-01 14:49:58 -0400

    useless stuff in app fixed

--- src/App.vue
+++ src/App.vue
@@ -10,33 +10,6 @@ import AppRegistration from "./components/AppRegistration.vue";
 import AppRegistrations from "./components/AppRegistrations.vue";
 
 export default {
-  data() {
-    return {
-      registrations: [],
-      users: [
-        { id: 1, name: "Max", registered: false },
-        { id: 2, name: "Anna", registered: false },
-        { id: 3, name: "Chris", registered: false },
-        { id: 4, name: "Sven", registered: false },
-      ],
-    };
-  },
-  computed: {
-    unregisteredUsers() {
-      return this.users.filter((user) => {
-        return !user.registered;
-      });
-    },
-  },
-  methods: {
-    userUnregistered(registration) {
-      const user = this.users.find((user) => {
-        return user.id == registration.userId;
-      });
-      user.registered = false;
-      this.registrations.splice(this.registrations.indexOf(registration), 1);
-    },
-  },
   components: {
     appRegistration: AppRegistration,
     appRegistrations: AppRegistrations,
